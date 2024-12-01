31d77abe7c9b93da044db085a21dc0ff9ac1e937
math.arantes <matheusoficial1411@gmail.com>
2024-12-01 10:07:36 -0300

    nav funcionando

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function interagirModal(modal){
         document.body.style.overflow='visible';
     }
 }
+
+//Script geral
+let dataAtual = new Date();
+let horaAtual = dataAtual.getHours();
+let minutoAtual = dataAtual.getMinutes();
+
+if(horaAtual >= 14 && horaAtual < 22){
+    if(((horaAtual == 14) && (minutoAtual >= 30)) || horaAtual > 14){
+        get('status').innerHTML="Aberto";
+    }
+    else{
+        get('status').innerHTML="Fechado";
+    }
+}
+else{
+    get('status').innerHTML="Fechado";
+}
