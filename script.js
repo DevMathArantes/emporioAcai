a0421d24c3a156b24f6fa2eefd79feefd54d4d08
math.arantes <matheusoficial1411@gmail.com>
2024-12-20 07:50:26 -0300

    teste final

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 //Listas
 
     let Doces = [
-        "Abacaxi R$ 3.50", "Nutella R$ 4.50", "Nutella x2 R$ 7.00",
-        "Aveia R$ 2.50", "Banana R$ 3.00", "Bis branco R$ 3.00",
-        "Bis preto R$ 3.00", "Castanha R$ 4.50", "Cereja R$ 6.00",
-        "Chantilly R$ 3.00", "Chocolate branco R$ 3.50", "Chocolate R$ 3.50",
-        "Cobertura de caramelo R$ 2.50", "Cobertura de chocolate R$ 2.50","Cobertura de morango R$ 2.50",
-        "Coco ralado R$ 3.00", "Confete R$ 3.50", "Creme de kit kat R$ 5.50",
-        "Creme de lacta com óreo R$ 5.00", "Creme de leite ninho R$ 5.00", "Creme de ouro branco R$ 4.50",
-        "Creme de valsa R$ 4.50", "Creme diamante negro R$ 5.00", "Creme laka R$ 5.00",
-        "Cupuaçu R$ 4.50", "Chocolate suflair R$ 3.99", "Creme de morango R$ 5.00",
-        "Creme de ovomaltine R$ 5.50", "Creme de paçoca R$ 3.99", "Creme de pistache R$ 6.50",
-        "Doce de leite R$ 2.50", "Farinha lactea R$ 3.00", "Ferrero rocher R$ 9.50",
-        "Goiabada R$ 3.00", "Granola R$ 3.00", "Granulado R$ 2.50",
-        "Kinder bueno R$ 9.50", "Kinder chocolate (ao leite) R$ 3.00", "Kinder ovo R$ 9.50",
-        "Kit kat R$ 4.99", "Kiwi R$ 5.00", "Leite condensado R$ 3.50",
-        "Leite Ninho R$ 4.00", "Mel R$ 2.50", "Morango R$ 4.00",
-        "Ouro branco R$ 3.00", "Ovomaltine R$ 3.50", "Paçoca R$ 2.75",
-        "Pasta de amendoim, sabor leite ninho R$ 4.99", "Pó de guaraná R$ 3.00", "Sonho de valsa R$ 3.00",
-        "Space boll R$ 3.50", "Sucrilhos R$ 3.00", "Whey protein R$ 4.99" 
+        "Abacaxi R$ 3.50", "Aveia R$ 2.50", "Banana R$ 3.00", 
+        "Bis branco R$ 3.00", "Bis preto R$ 3.00", "Chantilly R$ 3.00", 
+        "Chocolate branco R$ 3.50", "Chocolate R$ 3.50", "Cobertura de caramelo R$ 2.50", 
+        "Cobertura de chocolate R$ 2.50", "Cobertura de morango R$ 2.50", "Coco ralado R$ 3.00", 
+        "Confete R$ 3.50", "Mel R$ 2.50", "Farinha lactea R$ 3.00",
+        "Granola R$ 3.00", "Granulado R$ 2.50", "Kinder chocolate (ao leite) R$ 3.00",  
+        "Leite condensado R$ 3.50", "Doce de leite R$ 2.50", "Goiabada R$ 3.00",
+        "Paçoca R$ 2.75", "Pó de guaraná R$ 3.00", "Sonho de valsa R$ 3.00",
+        "Space boll R$ 3.50", "Sucrilhos R$ 3.00", "Ovomaltine R$ 3.50", 
+        "Ouro branco R$ 3.00",
+          
+        "Nutella x2 R$ 7.00", "Nutella R$ 4.50", "Castanha R$ 4.50",
+        "Cereja R$ 6.00", "Chocolate suflair R$ 3.99", "Creme de ovomaltine R$ 5.50", 
+        "Creme de paçoca R$ 3.99", "Creme de pistache R$ 6.50", "Leite Ninho R$ 4.00", 
+        "Morango R$ 4.00", "Cupuaçu R$ 4.50", "Creme de morango R$ 5.00",
+        "Creme de kit kat R$ 5.50", "Kit kat R$ 4.99", "Kiwi R$ 5.00",
+        "Pasta de amendoim, sabor leite ninho R$ 4.99", "Whey protein R$ 4.99", "Creme laka R$ 5.00",
+        "Creme de ouro branco R$ 4.50", "Creme de leite ninho R$ 5.00", "Creme de lacta com óreo R$ 5.00",
+        "Creme de valsa R$ 4.50", "Creme diamante negro R$ 5.00", 
+
+        "Ferrero rocher R$ 9.50", "Kinder bueno R$ 9.50", "Kinder ovo R$ 9.50"
     ];
 
     let Lanches = [ 
@@ -85,7 +88,9 @@
     function preencherLista(campo, lista, incluso){
         if(incluso){
             for(let i = 0; i < lista.length; i++){
-                get(campo).innerHTML+=`<option value="${lista[i].slice(0, -8)}">${lista[i].slice(0, -8)}</option>`;
+                if(parseFloat(lista[i].slice(-5)) < 3.99){
+                    get(campo).innerHTML+=`<option value="${lista[i].slice(0, -8)}">${lista[i].slice(0, -8)}</option>`;
+                }
             }
         }
         else{
@@ -103,10 +108,12 @@
 
     //Adiciona valores do select a descrição do produto
     function adicionarDescricao(identificador){
-        
-        
-       
-
+        let alteraValor = parseFloat(get('valor'+identificador).innerHTML)-parseFloat((get('descricao'+identificador).innerHTML).slice(-6));
+        get('valor'+identificador).innerHTML= alteraValor.toString();
+        get('descricao'+identificador).innerHTML = (get('descricao'+identificador).innerHTML).slice(0, -17);
+        get('descricao'+identificador).innerHTML += get('borda'+identificador).value;
+        alteraValor = parseFloat(get('valor'+identificador).innerHTML)+parseFloat((get('descricao'+identificador).innerHTML).slice(-6));
+        get('valor'+identificador).innerHTML= (alteraValor.toFixed(2)).toString();
     }
     
     //Muda o titulo e preço do produto de acordo com o tamanho
