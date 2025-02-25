31a402d1d180ca2ee8e8df2c6abf05c24ccddc30
math.arantes <matheusoficial1411@gmail.com>
2025-02-24 23:21:21 -0300

    correção

--- script.js
+++ script.js
@@ -1029,7 +1029,7 @@ function montarExtras(tipo, inclusos){
 
 //Abre o modal para personalizar o item desejado
 function pedir(id, ext, inclusos){
-    if(true){
+    if(false){
         dijuntor('montar');
         extras=[];
         get('montar').innerHTML=`
